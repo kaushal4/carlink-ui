da0d8e50820ba54b377a9099795dc34d1216e576
Kaushal Damania <damaniakaushal4@gmail.com>
2025-01-09 00:43:48 -0500

    progress

---
+++ app/components/nav.tsx
@@ -0,0 +1,10 @@
+
+
+export default () => {
+    return (
+        <nav className="flex items-center justify-between p-4 bg-gray-800 text-white">
+            <span className="text-xl font-bold">Car Link</span>
+
+        </nav>
+    );
+};
